4696a5f5aa1f57fa312e640289bf2ee15086fac1
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-01-07 18:17:51 +0100

    Changed some things

--- de.tubs.bibtextohtml.bibtex/src/de/tubs/bibtextohtml/bibtex/BibTeXRuntimeModule.java
+++ de.tubs.bibtextohtml.bibtex/src/de/tubs/bibtextohtml/bibtex/BibTeXRuntimeModule.java
@@ -3,9 +3,14 @@
  */
 package de.tubs.bibtextohtml.bibtex;
 
+import org.eclipse.xtext.conversion.IValueConverterService;
+import  de.tubs.bibtextohtml.bibtex.BibTeXTerminalConverters;
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class BibTeXRuntimeModule extends de.tubs.bibtextohtml.bibtex.AbstractBibTeXRuntimeModule {
-
+//    @Override
+//    public Class<? extends IValueConverterService> bindIValueConverterService() {
+//            return BibTeXTerminalConverters.class;
+//    }
 }
